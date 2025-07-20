4f2e0ad0968883a0d3b0532da552155f433d4c9e
giftedsession <oduorid606@gmail.com>
2025-07-20 17:04:20 +0300

    Update main-repo.js

--- plugins/main-repo.js
+++ plugins/main-repo.js
@@ -29,19 +29,19 @@ async (conn, mek, m, { from, reply }) => {
         const repoData = await response.json();
 
         // Format the repository information
-        const formattedInfo = `*BOT NAME:*\n> ${repoData.name}\n\n*OWNER NAME:*\n> ${repoData.owner.login}\n\n*STARS:*\n> ${repoData.stargazers_count}\n\n*FORKS:*\n> ${repoData.forks_count}\n\n*GITHUB LINK:*\n> ${repoData.html_url}\n\n*DESCRIPTION:*\n> ${repoData.description || 'No description'}\n\n*Don't Forget To Star and Fork Repository*\n\n> *© Powered By 𝐃𝐀𝐕𝐄-𝐗𝐌𝐃Tech 🖤*`;
+        const formattedInfo = `*BOT NAME:*\n> ${repoData.name}\n\n*OWNER NAME:*\n> ${repoData.owner.login}\n\n*STARS:*\n> ${repoData.stargazers_count}\n\n*FORKS:*\n> ${repoData.forks_count}\n\n*GITHUB LINK:*\n> ${repoData.html_url}\n\n*DESCRIPTION:*\n> ${repoData.description || 'No description'}\n\n*Don't Forget To Star and Fork Repository*\n\n> *© Powered By 𝐃𝐀𝐕𝐄-𝐗𝐌𝐃🖤*`;
 
         // Send image with caption
         await conn.sendMessage(from, {
-            image: { url: `https://files.catbox.moe/vn037q.jpg` },
+            image: { url: `120363400480173280@newsletter` },
             caption: formattedInfo,
             contextInfo: { 
                 mentionedJid: [m.sender],
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363419799432387@newsletter',
-                    newsletterName: '𝐃𝐀𝐕𝐄-𝐗𝐌𝐃Tech',
+                    newsletterJid: '120363400480173280@newsletter',
+                    newsletterName: '𝐃𝐀𝐕𝐄-𝐗𝐌𝐃',
                     serverMessageId: 143
                 }
             }
@@ -58,8 +58,8 @@ async (conn, mek, m, { from, reply }) => {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363419799432387@newsletter',
-                    newsletterName: '𝐃𝐀𝐕𝐄-𝐗𝐌𝐃Tech',
+                    newsletterJid: '120363400480173280@newsletter',
+                    newsletterName: '𝐃𝐀𝐕𝐄-𝐗𝐌𝐃',
                     serverMessageId: 143
                 }
             }
