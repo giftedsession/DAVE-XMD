8ac89dd5ce5ae5f0abdc8a877274e2c04f0de443
giftedsession <oduorid606@gmail.com>
2025-07-23 13:01:58 +0300

    Update _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -14,7 +14,7 @@ handler.all = async function (m) {
       }) * 1000;
     }
     let muptime = clockString(_muptime);
-    let bio = `\n⌚ Time Active ${muptime}\n\n ┃ 🛡️ DAVE-MD 🛡️`;
+    let bio = `\n⌚ Time Active ${muptime}\n\n ┃ 🛡️ DAVE-XMD 🛡️`;
     await this.updateProfileStatus(bio).catch(_ => _);
     setting.status = new Date() * 1;
   }
